5aff81d1eee321592b7cb4409786baa40f07788a
DroidWeb <67540237+droidweb20@users.noreply.github.com>
2020-06-29 20:19:17 +0300

    Added team-members.html and...
    ... a script that will generate team members

---
+++ generate-team-members.php
@@ -0,0 +1,102 @@
+<?php
+
+$members = '
+[
+    {
+        "name": "Roland Yan",
+        "image": "1",
+        "position": {
+            "long": "Chief Executive Officer",
+            "abbr": "CEO"
+        },    
+        "description": "Bacon ipsum dolor amet shankle brisket shank kevin porchetta hamburger sirloin chicken strip steak alcatra pancetta drumstick. Flank strip steak buffalo frankfurter biltong, jerky meatloaf. ",
+        "some": {
+            "website": "https://rolandyan.com",
+            "linkedin": "roland-yan-52618a1a7",
+            "instagram": "robo_panda_x"
+        }
+    },
+    {
+        "name": "Roja Das",
+        "image": "1",
+        "position": {
+            "long": "Chief Financial Officer",
+            "abbr": "CFO"
+        }    
+    },
+    {
+        "name": "Ishaan Panigrahi",
+        "image": "1",
+        "position": {
+            "long": "Chief Marketing Officer",
+            "abbr": "CMO"
+        }    
+    },
+    {
+        "name": "Tate Schrock",
+        "image": "1",
+        "position": {
+            "long": "Chief Technology Officer",
+            "abbr": "CTO"
+        }    
+    },
+    {
+        "name": "Ameya Singh",
+        "image": "1",
+        "position": {
+            "long": "Chief Website Officer",
+            "abbr": "CWO"
+        }    
+    },
+    {
+        "name": "Mac Kongsomjit",
+        "image": "1",
+        "position": {
+            "long": "Chief Operating Officer",
+            "abbr": "COO"
+        }    
+    },
+    {
+        "name": "Maksim Petersburgski",
+        "image": "1",
+        "position": {
+            "long": "Chief Information Officer",
+            "abbr": "CIO"
+        }    
+    },
+    {
+        "name": "Avani Guduri",
+        "image": "1",
+        "position": {
+            "long": "Chief Design Officer",
+            "abbr": "CDO"
+        }    
+    },
+    
+    
+        
+]
+';
+
+echo '
+                <div class="card beside">
+                    <div class="stacked">
+                        <img src="https://cdn.discordapp.com/attachments/706393501973348454/726896563418562590/image3.jpg" alt="Nakkia">
+                    </div>
+                    <div class="stacked">
+                        <p class="card-name">Roland Yan</p>
+                        <p class="card-position"><abbr title="Chief Executive Officer">CEO</abbr></p>
+                        <p class="card-description">Bacon ipsum dolor amet shankle brisket shank kevin porchetta hamburger sirloin chicken strip steak alcatra pancetta drumstick. Flank strip steak buffalo frankfurter biltong, jerky meatloaf. </p>
+                        <div class="card-some beside">
+                            <a href="https://rolandyan.com"><i class="fas fa-globe"></i></a>
+                            <a href="https://www.linkedin.com/in/roland-yan-52618a1a7/"><i class="fab fa-linkedin"></i></a>
+                            <a href="https://www.instagram.com/robo_panda_x/"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+';
+
+
+
+
+?>

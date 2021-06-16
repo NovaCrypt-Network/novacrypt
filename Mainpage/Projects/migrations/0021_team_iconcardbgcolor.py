2c748b8378e1d4153682a580e5dbb2b9b0deaa50
Maksim <maksim.peter777@gmail.com>
2021-06-16 13:17:12 -0400

    Update 0021_team_iconcardbgcolor.py

--- Mainpage/Projects/migrations/0021_team_iconcardbgcolor.py
+++ Mainpage/Projects/migrations/0021_team_iconcardbgcolor.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='team',
             name='IconCardBGColor',
-            field=models.CharField(default=django.utils.timezone.now, max_length=7),
+            field=models.CharField(default='FFFFFF', max_length=6),
             preserve_default=False,
         ),
     ]
